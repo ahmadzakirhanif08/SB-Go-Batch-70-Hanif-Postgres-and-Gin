7b7d30638b6a8428c139fe6142e9544d8634ce7c
hanifahmadzakir <hanifahmadzakir@gmail.com>
2025-09-26 22:48:42 +0800

    Modified main.go, modified database.go, and .env. adjust config for railway deployment

--- database/database.go
+++ database/database.go
@@ -1,26 +1,26 @@
 package database
 
 import (
+	"bioskop-api/models"
 	"fmt"
 	"log"
-	"bioskop-api/models"
+	"os"
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
-)
-
-const(
-	dbAddress = "localhost"
-	dbUser = "admin"
-	dbPassword = "admin"
-	dbName = "golang"
-	dbPort = "5432"
-	dbSslMode = "disable"
-	dbTimezone = "Asia/Jakarta"
 )
 
 var DB *gorm.DB
 
 func Connect() {
+
+	dbAddress := os.Getenv("DB_HOST")
+	dbUser := os.Getenv("DB_USER")
+	dbPassword := os.Getenv("DB_PASSWORD")
+	dbName := os.Getenv("DB_NAME")
+	dbPort := os.Getenv("DB_PORT")
+	dbSslMode := "disable"
+	dbTimezone := "Asia/Jakarta"
+
 	database := fmt.Sprintf(
 		"host=%s user=%s password=%s dbname=%s port=%s sslmode=%s TimeZone=%s",
 		dbAddress, dbUser, dbPassword, dbName, dbPort, dbSslMode, dbTimezone,
@@ -31,7 +31,7 @@ func Connect() {
 		log.Fatal("Database connection error, please check connection")
 	}
 
-	fmt.Printf("Successfully connected to database: %s",dbName)
-	
+	fmt.Printf("Successfully connected to database: %s", dbName)
+
 	DB.AutoMigrate(&models.Bioskop{}, &models.Film{}, &models.JadwalFilm{})
 }
--- main.go
+++ main.go
@@ -3,19 +3,31 @@ package main
 import (
 	"bioskop-api/database"
 	"bioskop-api/handlers"
+	"log"
+	"os"
+
 	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
 )
 
 func main() {
+
+	if err := godotenv.Load(); err != nil {
+		log.Fatal("error loading .env file")
+	}
+
 	database.Connect()
 
 	r := gin.Default()
+
+	basicAuthUser := os.Getenv("BASIC_AUTH_USER")
+	basicAuthPassword := os.Getenv("BASIC_AUTH_PASSWORD")
 
 	r.GET("/bioskop", handlers.AmbilSemuaBioskop)
 	r.GET("/bioskop/:id", handlers.AmbilBioskopByID)
 
 	authorized := r.Group("/", gin.BasicAuth(gin.Accounts{
-		"gaktau": "bilangwow",
+		basicAuthUser : basicAuthPassword,
 	}))
 	authorized.POST("/bioskop", handlers.TambahBioskop)
 	authorized.PUT("/bioskop/:id", handlers.PerbaharuiBioskop)
